8637ca0bb9c697c3b3e0674b82c5e92d40dffcd4
MD Soriful Islam <islamsoriful.hero@gmail.com>
2026-09-11 23:59:15 +0600

    Middlewares auth and error ratelimiter updated

--- src/middlewares/role.middleware.ts
+++ src/middlewares/role.middleware.ts
@@ -2,10 +2,6 @@ import { NextFunction, Request, Response } from "express";
 import { Role } from "@prisma/client";
 import { ApiError } from "../utils/ApiError";
 
-/**
- * Restrict a route to one or more roles. Must run after `authenticate`.
- * Usage: router.get("/", authenticate, authorize(Role.ADMIN), controller)
- */
 export function authorize(...allowedRoles: Role[]) {
   return (req: Request, _res: Response, next: NextFunction) => {
     if (!req.user) {
